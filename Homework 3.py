3d72c86b41e0462ae7027538fa96bcdd2d476508
ckbankhede <60286210+ckbankhede@users.noreply.github.com>
2026-06-08 20:13:24 -0500

    Add files via upload

---
+++ Homework 3.py
@@ -0,0 +1,13 @@
+text=("indexing")
+print(text[0])
+print(text[1])
+print(text[2])
+print(text[3])
+print(text[4])
+print(text[5])
+print(text[6])
+print(text[7])
+Text=str(input("Enter your string:"))
+rev=Text[::-1]
+Text=rev
+print(Text)
